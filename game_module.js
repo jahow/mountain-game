83f2f0e03c153f7b7eeccd7c01bf2625a3fd0e64
olivier.g <olivier@onirium-games.com>
2015-10-16 18:18:16 +0200

    switching code server side

---
+++ game_module.js
@@ -0,0 +1,16 @@
+
+// holds all players data
+var game_module = {
+
+	
+
+	function getPlayerData(id) {
+
+	}
+
+
+
+};
+
+
+module.exports = game_module;
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+var game_module = require('./game_module');
+
 /*
 var db_connection = require('./database');
 db_connection.connect( function(err){
@@ -29,9 +31,30 @@ db_connection.connect( function(err){
 
 app.use(express.static(__dirname + '/public'));
 
+// fetch gamestate from a spectator point of view
+app.get('/gamestate', function(req, res) {
+  console.log('gamestate requested from a spectator pov');
+  res.json({
+
+  });
+});
+
+// fetch gamestate from a particular player point of view
+app.get('/gamestate/:player_id', function(req, res) {
+  var player_id = req.params.player_id;
+  console.log('gamestate requested from a player pov');
+
+  var player_data = game_module.getPlayerData();
+
+  res.json({
+
+
+  });
+});
+
 app.use(function(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('Page introuvable !');
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(404).send('Page introuvable !');
 });
 
 
